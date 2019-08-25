1b9cc6ed9b3123933aa1d0d8a44b7f1955fc8129
DESKTOP-RKL89TS\mrsta <mrstark375@gmail.com>
2019-08-25 23:43:47 +0700

    Добавил реализацию функции printf через swo, очень рад что все заработало с первого раза

--- hardware/MDK-ARM/RTE/_reflow_oven/RTE_Components.h
+++ hardware/MDK-ARM/RTE/_reflow_oven/RTE_Components.h
@@ -16,5 +16,9 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_ITM       /* Compiler I/O: STDIN ITM */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
 
 #endif /* RTE_COMPONENTS_H */
