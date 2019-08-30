1681180cc1a8478de4c9b3898e0122e13eaa1905
tony-rpi <mrstark375@gmail.com>
2019-08-30 12:43:32 +0100

    Esche

--- rpi-uart-listener-cpp/my_software_stm32_crc.h
+++ rpi-uart-listener-cpp/my_software_stm32_crc.h
@@ -1,6 +1,8 @@
 #ifndef my_software_stm32_crc_h
 #define my_software_stm32_crc_h
 
+#include <stdint.h>
+
 const uint32_t CRC_INITIALVALUE = 0xFFFFFFFF;
 
 uint32_t stm32_sw_crc32_by_bit(uint32_t crc32, uint8_t pBuffer[], uint32_t NumOfByte);
