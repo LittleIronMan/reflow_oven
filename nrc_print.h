fd909f9bdc9e5e40badf62337291032bf5b2740d
DESKTOP-RKL89TS\mrsta <mrstark375@gmail.com>
2019-08-31 19:51:41 +0700

    Исправил ебучую ошибку, из-за которой компилятор gcc не хотел компилировать на линуске. Проблема решилась методом научного тыка, и это просто замечательно

--- nrc_print.h
+++ nrc_print.h
@@ -23,7 +23,7 @@ void nrcPrintfEx(uint8_t viewMode, char *fmt, ...);
 
 #if NRC_LOG_LEVEL >= NRC_LOG_LEVEL_DEBUG
 #define nrcPrintfD(...) nrcPrintfEx(0, __VA_ARGS__)
-#define nrcLogD(...) nrcPrintfEx(NRC_LOG_ADD_COUNTER | NRC_LOG_ADD_EOL, __VA_ARGS__)
+#define nrcLogD(...); nrcPrintfEx(NRC_LOG_ADD_COUNTER | NRC_LOG_ADD_EOL, __VA_ARGS__)
 #else
 #define nrcPrintfD(...)
 #define nrcLogD(...)
