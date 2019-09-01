69fb4675d06da0759cf3092a44229c4bf66e1fb1
DESKTOP-RKL89TS\mrsta <mrstark375@gmail.com>
2019-09-02 00:07:54 +0700

    Первая реализация графика(пока синусоида рисуется)

--- web/app/pages/main/PageMain.jsx
+++ web/app/pages/main/PageMain.jsx
@@ -28,13 +28,60 @@ class ControlButtons extends Component {
     }
 }
 
+class GraphView extends Component {
+    constructor(props) {
+        super(props);
+        this.drawGraph = this.drawGraph.bind(this);
+    }
+
+    drawGraph() {
+        var canvas = document.getElementById("surface");
+        var context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        context.beginPath();
+        var lines = 200,
+            frag = canvas.width / lines,
+            scale = canvas.height / 2;
+        console.log(`Canvas client size: ${canvas.clientWidth}x${canvas.clientHeight}`);
+        console.log(`Canvas size: ${canvas.width}x${canvas.height}`);
+        console.log("frag: ", frag);
+        console.log("scale: ", scale);
+
+        context.moveTo(0, scale);
+        for (var i = 0; i < lines; i++) {
+            var sine = Math.sin(2*3.141592 * i/lines)*scale;
+
+            context.lineTo(i*frag, -sine+scale);
+        }
+        context.stroke();
+    }
+
+    componentDidUpdate() {
+        this.drawGraph();
+    }
+    componentDidMount() {
+        this.drawGraph();
+        window.addEventListener("resize", this.drawGraph);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.drawGraph);
+    }
+
+    render() {
+        return <canvas id="surface" className={style.graphView}>
+
+        </canvas>
+    }
+}
+
 class PageMain extends Component {
     render() {
         return <div className={page.default}>
             Oven Control Panel
             <TempMonitor/>
             <ControlButtons/>
-            <canvas className={style.graphView}></canvas>
+            <GraphView/>
         </div>;
     }
 }
