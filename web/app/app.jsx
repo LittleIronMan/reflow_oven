7d292c2e3d796542965680a00df0898015e1b8d6
RoE_Mishka <mrstark375@gmail.com>
2019-09-02 13:59:27 +0700

    Интегрировал (за пол-дня) webpack-hot-middleware, теперь веб-страница будет перезагружаться на лету после каких-либо изменений в файлах

--- web/app/app.jsx
+++ web/app/app.jsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom';
 import MainPage from 'pages/main/PageMain.jsx';
 import 'styles/main.scss';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 ReactDOM.render(
     <MainPage/>,
     document.getElementById('app')
