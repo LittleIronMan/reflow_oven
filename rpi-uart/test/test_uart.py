40735d6189c18893d1f66ec2081a1d0d6915619e
DESKTOP-RKL89TS\mrsta <mrstark375@gmail.com>
2019-10-07 23:47:14 +0700

    Подкорректировал в питоновском скрипте реакции на клавиши

--- rpi-uart/test/test_uart.py
+++ rpi-uart/test/test_uart.py
@@ -1,51 +1,62 @@
 #!/usr/bin/env python3
 # реакция на нажатия клавиш позаимствовано отсюда: https://rosettacode.org/wiki/Keyboard_input/Keypress_check#Python
-from __future__ import absolute_import, division, unicode_literals, print_function
+
+# from __future__ import absolute_import, division, unicode_literals, print_function
  
-import tty, termios
-import sys
-if sys.version_info.major < 3:
-    import thread as _thread
-else:
-    import _thread
-import time
- 
- 
+isLinux = False
+isWindows = False
+
 try:
-    from msvcrt import getch  # try to import Windows version
-except ImportError:
-    def getch():   # define non-Windows version
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+	import tty, termios
+	isLinux = True
+except:
+	try:
+		import msvcrt
+		isWindows = True
+	except:
+		pass
+
+import sys, _thread, time
+ 
+if isWindows:
+	from msvcrt import getch  # try to import Windows version
+elif isLinux:
+	def getch():   # define non-Windows version
+		fd = sys.stdin.fileno()
+		old_settings = termios.tcgetattr(fd)
+		try:
+			tty.setraw(sys.stdin.fileno())
+			ch = sys.stdin.read(1)
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+		return ch
+else:
+	def getch():
+		return None
  
 def keypress():
-    global char
-    char = getch()
+	global char
+	char = getch()
  
 def main():
-    global char
-    char = None
-    _thread.start_new_thread(keypress, ())
+	global char
+	char = None
+	_thread.start_new_thread(keypress, ())
  
-    while True:
-        if char is not None:
-            try:
-                print("Key pressed is " + char.decode('utf-8'))
-            except UnicodeDecodeError:
-                print("character can not be decoded, sorry!")
-                char = None
-            _thread.start_new_thread(keypress, ())
-            if char == 'q' or char == '\x1b':  # x1b is ESC
-                exit()
-            char = None
-        print("Program is running")
-        time.sleep(1)
+	while True:
+		if char is not None:
+			try:
+				char = char.decode('utf-8')
+				print("Key pressed is " + char)
+			except UnicodeDecodeError:
+				print("character can not be decoded, sorry!")
+				char = None
+			if char == 'q' or char == '\x1b':  # x1b is ESC
+				exit()
+			char = None
+			_thread.start_new_thread(keypress, ())
+		#print("Program is running")
+		time.sleep(0.1)
  
 if __name__ == "__main__":
-    main()
+	main()
